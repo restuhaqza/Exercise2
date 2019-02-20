f09c8ee1df4c4c766a82e02ea2c2e6c8dd05b7cb
restuhaqza <restuhaqza@gmail.com>
2019-02-20 20:59:35 +0700

    calculator layout

---
+++ assets/js/custom.js
@@ -0,0 +1,10 @@
+function trigButton(value){
+    var form = document.getElementById('txtCalculation')
+    console.log(parseFloat(form.value))
+    console.log(form.value)
+    console.log(value)
+}
+
+function equalButton(){
+
+}
